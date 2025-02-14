2f0ed46d48137d94401b9c82fe73a90e0634a809
wired87 <132303650+wired87@users.noreply.github.com>
2025-02-14 07:40:33 +0100

    Update firebase.config.js

--- firebase.config.js
+++ firebase.config.js
@@ -3,23 +3,7 @@ import {getReactNativePersistence, initializeAuth} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 
-const firebaseConfig = {
-
-    apiKey: "AIzaSyAaQvHP9Syxu-ycHqXCdHcGtTGEK-vczjc",
-
-    authDomain: "thehub-66eb1.firebaseapp.com",
-
-    projectId: "thehub-66eb1",
-
-    storageBucket: "thehub-66eb1.appspot.com",
-
-    messagingSenderId: "395573890829",
-
-    appId: "1:395573890829:web:5fb7f187df79fdbbbdea44",
-
-    measurementId: "G-5X4QZ6QCEY"
-
-};
+const firebaseConfig = // Get from your firebase proj
 
 // Initialize Firebase
 export const FIREBASE_APP = initializeApp(firebaseConfig);
